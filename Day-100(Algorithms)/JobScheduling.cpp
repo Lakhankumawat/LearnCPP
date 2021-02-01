02a3a51c9fa014d969ffa1b7619b8abad038ff5f
LakhanKumawat <55774240+Lakhankumawat@users.noreply.github.com>
2021-02-01 14:50:15 +0530

    Update JobScheduling.cpp

--- Day-100(Algorithms)/JobScheduling.cpp
+++ Day-100(Algorithms)/JobScheduling.cpp
@@ -1,3 +1,4 @@
+//GDB CODE LINK Compile and Execute : https://onlinegdb.com/rknD8SBeO
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -20,7 +21,7 @@ bool comparison(Job a, Job b)
 void JobSchedulingUsingDeadline(Job a[],int n, int finaldead){
 sort(a,a+n,comparison);
 
-int result[n];
+int result[n],totalprofit=0;
 bool slot[n];
 
 for(int j=0;j<n;j++)
@@ -28,34 +29,40 @@ for(int j=0;j<n;j++)
 
 for(int i=0;i<n;i++){
 
-   for(int j=min(n,finaldead)-1;j>=0;j--){
+   for(int j=min(n,a[i].deadL)-1;j>=0;j--){
     if(slot[j]==false){
-        slot[j]=true;
-        result[j]=i;
-        break;
+        totalprofit+=a[i].profit;
+         result[j] = i;  // Add this job to result
+             slot[j] = true; // Make this slot occupied
+             break;
     }
    }
 }
-
+cout<<"Jobs Sequencing Order : ";
 for(int o=0;o<n;o++)
-    if(slot[o])
-    cout<< a[result[o]].JobNo+1<<endl;
-
+    if(slot[o]){
+    cout<< a[result[o]].JobNo+1<<" ";
+    }
+    cout<<"Total Profit : "<<totalprofit;
 }
 
 int main(){
  int no,finaldead;
-    cout<<"Enter Your Deadline: ";
-   cin>>finaldead;
+
     cout<<"Enter Total Jobs: ";
 cin>>no;
+cout<<"Enter Your Final Deadline: ";
+   cin>>finaldead;
 Job Arr[no];
+cout<<" Profit  Deadline"<<"\n";
 for(int i=0;i<no;i++){
 
-   cout<<" Profit "<<i+1<<" : "; cin>>Arr[i].profit;
-     cout<<" Deadline "<<i+1<<" : "; cin>>Arr[i].deadL;
+
+  cin>>Arr[i].profit>>Arr[i].deadL;
      Arr[i].JobNo = i;
 }
 
 JobSchedulingUsingDeadline(Arr,no,finaldead);
 }
+
+
