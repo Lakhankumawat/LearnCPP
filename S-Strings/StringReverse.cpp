b7034b145315608437a8ebf403e8f13b026f0168
Rutuja Ingole <rutujaingole23@gmail.com>
2021-03-05 13:40:19 +0530

    program for reversing the string elements

---
+++ S-Strings/StringReverse.cpp
@@ -0,0 +1,26 @@
+//program to reverse a string using recursion in C++
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+#function to reverse a string
+void reverse(char *x, int begin, int end)
+{
+char c;
+if (begin >= end)
+return;
+c = *(x + begin);
+*(x + begin) = *(x + end);
+*(x + end) = c;
+reverse(x, ++begin, --end);
+}
+
+int main()
+{
+char a[100];
+cin >> a;
+reverse(a, 0, strlen(a) - 1);
+cout << a;
+return 0;
+}
