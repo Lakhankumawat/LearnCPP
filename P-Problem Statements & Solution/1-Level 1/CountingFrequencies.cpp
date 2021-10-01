7b73daac80f312c49c4ae9709d3de4c4343582cb
Lucifer4255 <50608734+Lucifer4255@users.noreply.github.com>
2021-10-01 18:55:26 +0530

    First Commit
    Problem statement is to find frequency of all elements in an array in O(n) time complexity

---
+++ P-Problem Statements & Solution/1-Level 1/CountingFrequencies.cpp
@@ -0,0 +1,31 @@
+// The problem is to count frequencies of all elements in an array in O(n) time complexity
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void countFreq(int arr[], int n)
+{
+    unordered_map<int, int> mp;
+ 
+    // Traverse through array elements and
+    // count frequencies
+    for (int i = 0; i < n; i++)
+        mp[arr[i]]++;
+ 
+    // Traverse through map and print frequencies
+    for (auto x : mp)
+        cout << x.first << " " << x.second << endl;
+}
+ 
+int main()
+{
+    int n;
+    cout<<"Enter the number of Elements in an array = ";
+    cin >> n;
+    int arr[n+1];
+    for(int i =0;i<n;i++){
+        cin >> arr[i];
+    }
+    countFreq(arr, n);
+    return 0;
+}
