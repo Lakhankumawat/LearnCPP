6cf65d9be59e736c9bcde195956077dbec5679f1
Sai Aditya Viswanadham <71250738+saiadityaviswanadham@users.noreply.github.com>
2022-03-01 20:07:12 +0530

    Create johnsonalgorithm.cpp
    Created johnsonalgorithm.cpp

---
+++ G-GraphAlgorithms/S-SingleSourceShortestPath/johnsonalgorithm.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define INF 9999
+using namespace std;
+int min(int a, int b);
+int cost[10][10], adj[10][10];
+inline int min(int a, int b){
+   return (a<b)?a:b;
+}
+main() {
+   int vert, edge, i, j, k, c;
+   cout << "Enter no of vertices: ";
+   cin >> vert;
+   cout << "Enter no of edges: ";
+   cin >> edge;
+   cout << "Enter the EDGE Costs:\n";
+   for (k = 1; k <= edge; k++) { //take the input and store it into adj and cost matrix
+      cin >> i >> j >> c;
+      adj[i][j] = cost[i][j] = c;
+   }
+   for (i = 1; i <= vert; i++)
+      for (j = 1; j <= vert; j++) {
+         if (adj[i][j] == 0 && i != j)
+            adj[i][j] = INF; //if there is no edge, put infinity
+      }
+   for (k = 1; k <= vert; k++)
+      for (i = 1; i <= vert; i++)
+         for (j = 1; j <= vert; j++)
+            adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]); //find minimum path from i to j through k
+   cout << "Resultant adj matrix\n";
+   for (i = 1; i <= vert; i++) {
+      for (j = 1; j <= vert; j++) {
+            if (adj[i][j] != INF)
+               cout << adj[i][j] << " ";
+      }
+      cout << "\n";
+   }
+}
