acba7160ca5cb73add02c7012de9779ede71e481
Priyanshu Saxena <priyanshusaxena2612@gmail.com>
2022-03-01 22:16:25 +0530

    [01/03] Modified to Dutch National Flag Algorithm

--- A-Array/Sort_0_1_2.cpp
+++ A-Array/Sort_0_1_2.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 using namespace std;
 
-
 // method to swap elements
 void swapElements(int *a, int *b)
 {
@@ -20,66 +19,35 @@ void printSortedArray(vector<int> array)
     }
 }
 
-
-// first method to sort the array using count method
-vector<int> sortArray1(vector<int> array)
-{
-    int count0 = 0, count1 = 0, count2 = 0;
-    for (auto element : array)
-    {
-        if (element == 0)
-        {
-            count0++;
-        }
-        else if (element == 1)
-        {
-            count1++;
-        }
-        else
-        {
-            count2++;
-        }
-    }
-    int iterator = 0;
-    while (count0 > 0)
-    {
-        array[iterator++] = 0;
-        count0--;
-    }
-    while (count1 > 0)
-    {
-        array[iterator++] = 1;
-        count1--;
-    }
-    while (count2 > 0)
-    {
-        array[iterator++] = 2;
-        count2--;
-    }
-    return array;
-    // Time Complexity: O(n) -> Parsing through each element of the array
-    // Space Coplexity: O(1) -> No extra space used.
-}
-
-
-// second method to sort the array using only 3 variables and single traversal
-vector<int> sortArray2(vector<int> array)
+/*
+    Method to sort the array using only 3 variables and single traversal
+    Also known as Dutch National Flag Algorithm.
+*/
+vector<int> sortArray(vector<int> array)
 {
     int low = 0;
     int mid = 0;
     int high = array.size() - 1;
+    //  Taking 3 ranges:
+    //  1 to low will contain only 0s
+    //  low to mid will contain only 1s
+    //  mid to high will contain ony 2s
     while (mid <= high)
     {
+        // case 1: if the mid is pointing to 0, then swap the array values of low and high and
+        // increment both low and mid
         if (array[mid] == 0)
         {
             swapElements(&array[low], &array[mid]);
             low++;
             mid++;
         }
+        // case 2: if the mid is pointing to 1, then increrment only mid
         else if (array[mid] == 1)
         {
             mid++;
         }
+        // case 3: if the mid is pointing to 2, then swap mid and high and then decrease high
         else
         {
             swapElements(&array[mid], &array[high]);
@@ -91,14 +59,12 @@ vector<int> sortArray2(vector<int> array)
     // Space Complexity: O(1)
 }
 
+// main function
 int main()
 {
     vector<int> array = {0, 1, 1, 0, 2, 0, 1, 1, 2};
-    vector<int> sortedArray1 = sortArray1(array);
-    vector<int> sortedArray2 = sortArray2(array);
-    cout << "Sorted array with first method: ";
-    printSortedArray(sortedArray1);
-    cout << "\nSorted array with second method: ";
-    printSortedArray(sortedArray2);
+    vector<int> sortedArray = sortArray(array);
+    cout << "Sorted array is: ";
+    printSortedArray(sortedArray);
     return 0;
 }
