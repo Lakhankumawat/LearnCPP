14ae501098b383bdbff075fd1777d9d9e0ccb77f
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2022-03-03 20:27:26 +0530

    Update Fisher_Yates_Algorithm.cpp

--- S-Sequences/Fisher_Yates_Algorithm.cpp
+++ S-Sequences/Fisher_Yates_Algorithm.cpp
@@ -7,46 +7,34 @@
 #include <time.h>
 using namespace std;
 
-// A utility function to swap to integers
-void swap (int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
 
-// A utility function to print an array
-void printArray (int arr[], int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << "\n";
-}
-
-// A function to generate a random permutation of arr[]
-void randomize (int arr[], int n)
+void randomize (int Arr[], int num)
 {
 	//Use a different seed value so that we don't get same result each time this program is run,
 	srand (time(NULL));
 
 	// Start from the last element and swap one by one. We don't need to run for the first element that's why i > 0
-	for (int i = n - 1; i > 0; i--)
+	for (int i = num - 1; i > 0; i--)
 	{
 		// Pick a random index from 0 to i
 		int j = rand() % (i + 1);
 
 		// Swap arr[i] with the element at random index
-		swap(&arr[i], &arr[j]);
+		swap(&Arr[i], &Arr[j]);
 	}
 }
 
 
 int main()
 {
-	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = {10,9,67,23,45};
+	int n=arr.size();
 	randomize (arr, n);
-	printArray(arr, n);
+	for(int t=0;t<n;t++)
+	{
+		cout<<arr[t]<<" ";
+	}
+	cout<<endl;
 
 	return 0;
 }
