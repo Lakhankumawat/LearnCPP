332393e13f97ce3fed53bf3a81f9e1830f225943
Vinayak Buchke <72989303+Vinayakk404@users.noreply.github.com>
2022-03-04 12:37:13 +0530

    Update Fibonacci_series.cpp
    Changes done
    - Indented the code
    -Corrected spelling
    -Comment and space/time complexity removed

--- D-DynamicProgramming/Fibonacci_series.cpp
+++ D-DynamicProgramming/Fibonacci_series.cpp
@@ -1,52 +1,47 @@
 // The Fibonacci numbers are the numbers in the following integer sequence. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..and so on
 
 // C++ program for Fibonacci Series using Dynamic Programming upto nth term
-
-
 #include <iostream>
 using namespace std;
 
 void fibonacci(int n)
 {
-  int F[n]; //Declaring integer array of size n in which we will store and display our numbers
-  F[0]=0; 
-  F[1]=1;
-  if (n==1) {
-    cout<<F[0];  //printing the first term 
-  }  
-
-  else if(n==2) {
-     cout<<F[0]<<","<<F[1];  //printing the first two terms
-      
-  } 
-
-  else {
-
-     cout<<F[0]<<","<<F[1];
-
-  for(int i=2;i<n;i++){
-     F[i]=F[i-1]+F[i-2];  // adding preceeding two terms to get the next term
-     cout<<","<<F[i]; 
-    }
-  }
+   int F[n];	//Declaring integer array of size n in which we will store and display our numbers
+   F[0] = 0;
+   F[1] = 1;
+   if (n == 1)
+   {
+      cout << F[0];	//printing the first term 
+   }
+   else if (n == 2)
+   {
+      cout << F[0] << "," << F[1];	//printing the first two terms
+   }
+   else
+   {
+      cout << F[0] << "," << F[1];
+      for (int i = 2; i < n; i++)
+      {
+         F[i] = F[i - 1] + F[i - 2];	// adding preceeding two terms to get the next term
+         cout << "," << F[i];
+      }
+   }
 }
 
- //Driver meathod
+//Driver method
 int main()
 {
    int n;
-   cout<<"Enter the value of n"<<endl;
-   cin>>n;
-   if (n<1){              //as the series starts from 1st term
-     cout<<"Value of n should be greater than 0";  
-   } 
-   else{
-     cout<<"Required fibonacci series is ";
-     fibonacci(n);
-
+   cout << "Enter the value of n" << endl;
+   cin >> n;
+   if (n < 1)
+   {
+      cout << "Value of n should be greater than 0";
    }
-    return 0;
+   else
+   {
+      cout << "Required fibonacci series is ";
+      fibonacci(n);
+   }
+   return 0;
 }
-
-//The space complexity is O(n)
-//The time complexity is  O(n)
