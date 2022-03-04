3c8852260d97b0d6c42d5daaaf386bda552bf160
Aisha <100334178+i-am-aisha@users.noreply.github.com>
2022-03-04 15:07:41 +0530

    PriorityQueue.cpp
    Add some comments containing helpful information for getting the concept of priority queue in much better way.

--- P-PriorityQueue/PriorityQueue.cpp
+++ P-PriorityQueue/PriorityQueue.cpp
@@ -6,11 +6,15 @@ using namespace std;
 int main(){
 
 /* priority Queue are included in queue header file */
+    
+    /*By default priority queue holds data in descending order so for holding data in ascending order we have to initialise priority queue
+    like this-->priority_queue<int,vector<int>,greater<int> > pq;*/
+    
 
 /*priority_queue<int> pq; */ //max priority queue - Print in descending order
 priority_queue<int,vector<int>,greater<int> > pq; //min priority queue -ascending order
-int n,no;
-cin>>n;
+int n,no;  //declaring variables of integer (int) type
+cin>>n;      //taking inputs
 
 
 for(int i=1;i<=n;i++)
@@ -19,8 +23,8 @@ for(int i=1;i<=n;i++)
 
     //Higher no has high priority over here
     while(!pq.empty()){
-        cout<<pq.top()<<" ";
-        pq.pop();
+        cout<<pq.top()<<" ";        //printing pq-> top element
+        pq.pop();                   //popping out the top element from pq
     }
 
 return 0;
