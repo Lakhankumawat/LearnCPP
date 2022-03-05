d3886fbac5a1131827f9e2c99d414f9ee8ab24be
Priyanshu Saxena <priyanshusaxena2612@gmail.com>
2022-03-05 14:57:21 +0530

    [05/03] Edited README and Sort_0_1_2.cpp

--- A-Array/README.md
+++ A-Array/README.md
@@ -1,29 +1,52 @@
-## [1. Sort an Array of 0s, 1s and 2s](https://github.com/PriyanshuSaxena2612/LearnCPP/blob/WorkOnBit/A-Array/Sort_0_1_2.cpp)
+<!-- Table of content -->
+# Table of content
+- [Table of content](#table-of-content)
+  - [1. Sort an Array of 0s, 1s and 2s](#1-sort-an-array-of-0s-1s-and-2s)
+    - [Dutch National Flag Algorithm](#dutch-national-flag-algorithm)
+    - [Properties](#properties)
+    - [Sample Output](#sample-output)
+
+
+## [1. Sort an Array of 0s, 1s and 2s](https://github.com/Lakhankumawat/LearnCPP/blob/main/A-Array/Sort_0_1_2.cpp)
 
 ![Snap](https://user-images.githubusercontent.com/53018678/156426951-61050466-bf4a-4685-be42-e81a5cf623cb.png)
 
 
-Given an array A[] consisting 0s, 1s and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first, then all 1s and all 2s in last.
+> Given an array consisting 0s, 1s and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first, then all 1s and all 2s in last.
 <hr>
 
-## Dutch National Flag Algorithm
+### Dutch National Flag Algorithm
 ```
-1. Keep three indices low = 1, mid = 1 and high = N and there are four ranges, 1 to low (the range containing 0), low to mid (the range containing 1), mid to high (the range containing unknown elements) and high to N (the range containing 2).
-2. Traverse the array from start to end and mid is less than high. (Loop counter is i)
-3. If the element is 0 then swap the element with the element at index low and update low = low + 1 and mid = mid + 1
-4. If the element is 1 then update mid = mid + 1
-5. If the element is 2 then swap the element with the element at index high and update high = high – 1 and update i = i – 1. As the swapped element is not processed
-6. Print the array.
+begin sortArray(array)
+   int low = 0
+   int mid = 0
+   int high = array.size() - 1
+
+   while(mid <= high)
+      if (array[mid] == 0)
+         swap(array[low], array[mid])
+         low++
+         mid++
+      else if (array[mid] == 1)
+         mid++
+      else
+         swap(array[mid], array[high])
+         high--
+      end if
+
+    return array
+
+end sortArray
 ```
    <hr>
 
 ### Properties
-* Space Complexity: O(1)
 * Time Complexity: O(n)
+* Space Complexity: O(1)
 * In-Place: Yes
 * Stable: Yes
 
-### My OUTPUT
+### Sample Output
 ![Snap2](https://user-images.githubusercontent.com/53018678/156428828-324bda09-356d-47dc-bfaa-793bae0a3dff.png)
 
 
--- A-Array/Sort_0_1_2.cpp
+++ A-Array/Sort_0_1_2.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-// method to swap elements
-void swapElements(int *a, int *b)
-{
-    *a ^= *b;
-    *b ^= *a;
-    *a ^= *b;
-}
-
-// method to print the sorted array
+// method to print the array
 void printArray(vector<int> array)
 {
     for (auto element : array)
@@ -28,18 +21,20 @@ vector<int> sortArray(vector<int> array)
     int low = 0;
     int mid = 0;
     int high = array.size() - 1;
+
     //  Taking 4 ranges:
     //  1 to low will contain only 0s
     //  low to mid will contain only 1s
     //  mid to high will contain unknown elements
     //  high to size of array will contain 2s
+
     while (mid <= high)
     {
         // case 1: if the mid is pointing to 0, then swap the array values of low and high and
         // increment both low and mid
         if (array[mid] == 0)
         {
-            swapElements(&array[low], &array[mid]);
+            swap(array[low], array[mid]);
             low++;
             mid++;
         }
@@ -51,7 +46,7 @@ vector<int> sortArray(vector<int> array)
         // case 3: if the mid is pointing to 2, then swap mid and high and then decrease high
         else
         {
-            swapElements(&array[mid], &array[high]);
+            swap(array[mid], array[high]);
             high--;
         }
     }
@@ -63,28 +58,51 @@ vector<int> sortArray(vector<int> array)
 // main function
 int main()
 {
+    vector<int> array;
+    int totalInputs;
+    cout << "Enter the number of inputs for the array: \n";
+    cin >> totalInputs;
+    cout << "Enter " << totalInputs << " elements in the array:\n";
+    while (totalInputs--)
+    {
+        int inputElement;
+        cin >> inputElement;
+        array.push_back(inputElement);
+    }
+    vector<int> sortedArray = sortArray(array);
+    cout << "Unsorted Array is: ";
+    printArray(array);
+    cout << "\nSorted array is: ";
+    printArray(sortedArray);
+
     // Test case 1
+    /*
     vector<int> array1 = {0, 1, 2, 2, 1, 1, 1};
     vector<int> sortedArray1 = sortArray(array1);
     cout << "Unsorted Array for test case 1: ";
     printArray(array1);
     cout << "\nSorted array for test case 1: ";
     printArray(sortedArray1);
+    */
 
     // Test case 2
+    /*
     vector<int> array2 = {0, 1, 0, 2, 1, 1, 1};
     vector<int> sortedArray2 = sortArray(array2);
     cout << "\nUnsorted Array for test case 2: ";
     printArray(array2);
     cout << "\nSorted array for test case 2: ";
     printArray(sortedArray2);
+    */
 
     // Test case 3
+    /*
     vector<int> array3 = {2, 1, 1, 0, 1, 2, 2, 1, 1, 1};
     vector<int> sortedArray3 = sortArray(array3);
     cout << "\nUnsorted Array for test case 3: ";
     printArray(array3);
     cout << "\nSorted array for test case 3: ";
     printArray(sortedArray3);
+    */
     return 0;
 }
