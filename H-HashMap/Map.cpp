715e14b27e4eab821a833c52f195dbd5b2863c79
Izhar <izharhamdan@gmail.com>
2022-03-07 00:19:55 +0530

    Updated Map.cpp and README.md

--- H-HashMap/Map.cpp
+++ H-HashMap/Map.cpp
@@ -1,58 +1,48 @@
 
+// Please read the README.md before going through the code for better understanding for maps 
 
+
+#include<iostream>
+#include<string>
+#include<map> // The header file specifically made for STL-Maps
 
 #include<bits/stdc++.h> // A High utility header file generally used in Competitive Programming contains 100's header file
 
 using namespace std;
-
-// MAPS is a Data Structre which stores KEY-VALUE Pairs 
-// Ordered Maps use Red Black Trees : an advanced data structre : https://www.geeksforgeeks.org/red-black-tree-set-1-introduction-2/
-// maps are not contious Example it++ is allowed while it+1 is not  
-// All keys are unique if redeclaration happens the pair with that pre-existing key updates
-
-// READ MORE ABOUT MAPS : 
-
-// 1. https://www.geeksforgeeks.org/map-associative-containers-the-c-standard-template-library-stl/
-
-// 2. https://www.cplusplus.com/reference/map/map/
-
 
 void print(map<int,string> &m)
 {
   cout<<m.size()<<endl;
 for( auto &pr : m)
 {
-  cout<<pr.first<<" "<<pr.second<<endl;
+  cout<<pr.first<<" "<<pr.second<<endl;  // printing takes O(logn) , since we run the loop 'n' times so final complexity becomes O(nlogn)-> TIME COMPLEXITY
 }
-
-// printing also O(logn) : TIME COMPLEXITY
 
 }
 
 int main()
 {
 
-map<int,string> m; // Declaration Syntax of Map STL 
+map<int,string> m; // Declaration syntax of STL-Map 
 
-m[1]="abc"; //O(logn) ALSO DEPENDS ON KEY ALSO ( Refer to RED BLACK TREES ) : TIME COMPLEXITY
+m[1]="abc"; 
 m[5]="cdc";
 m[3]="acd";
 
-// Red Black Trees : It compares other strings length and place it acc to sorted method , READ more on above link
-// string comparison time is added therefore
-// Final TIME COMPLEXITY {s.size()*O(logn)} 
+//Insertion takes O(logn) : It depends on the key also ( Refer to RED BLACK TREES ) -> TIME COMPLEXITY
 
-auto it1=m.find(3); // returns iterator of 3 if it of 3 doesnt exist then retunr m.end()
-if(it1==m.end()){
-  cout<<"NO VALUE"<<endl;
+auto it1=m.find(3); // returns iterator of the value '3' and stores it in it1 
+
+if(it1==m.end()){  // if '3' is not present then it1 will be assigned with m.end() which has NO VALUE 
+  cout<<"NO VALUE"<<endl; 
 }
 else{
   cout<<(*it1).first<<" "<<(*it1).second<<endl;
 }
 
 
-if(it1!=m.end()) // Checking if 'it' exist or not to avoid Segmentation Fault: https://www.geeksforgeeks.org/core-dump-segmentation-fault-c-cpp/
-m.erase(it1); //O(logn) one must never give any iterator that doesnt exist else will give segmentation fault : TIME COMPLEXITY
+if(it1!=m.end()) 
+m.erase(it1); //O(logn)  -> TIME COMPLEXITY
 
 print(m); 
 
@@ -74,15 +64,6 @@ for(it=m.begin();it!=m.end();it++)
 
 }
 
-
-
-// strings are also stored in lexographical order ie dictionary order
-
-
-
-
 print(m);
-
-
 
 }
