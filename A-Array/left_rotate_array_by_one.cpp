a5b923ceb4014354145ae5b54c3fcd1abf9eee58
mehakk-Bhatia <91878394+mehakk-Bhatia@users.noreply.github.com>
2022-03-07 16:24:44 +0530

    Update left_rotate_array_by_one.cpp

--- A-Array/left_rotate_array_by_one.cpp
+++ A-Array/left_rotate_array_by_one.cpp
@@ -12,13 +12,15 @@ int main()
 	}
 	cout<<endl;
 	cout<<"After rotation: "<<endl;   //After Rotation of array
-	int temp = Arr[0];
+	int temp = Arr[0];         // assigning element at 0 index to a variable temp
 	for (int j =1 ; j<n ; j++) {
-		Arr[j-1] = Arr[j];
+		Arr[j-1] = Arr[j]; 
+		//assigning element of next index to previous index     for ex: Arr[0] = Arr[1] i.e at index 0 the element of index 1 will be stored which is 2.
+		//this implies Arr[0] = 2 , Similarly Arr[1] = 3 and so on.
 	}
-	Arr[n-1] = temp;
+	Arr[n-1] = temp; //  the temp is assigned to index 5. this implies Arr[5] = temp and temp = Arr[0] i.e = 1 . this means Arr[5] = 1
 	for (int j=0 ; j< n ; j++) {
-		cout<<Arr[j]<<" ";
+		cout<<Arr[j]<<" ";   // hence after rotataing the array will be equal to {2,3,4,5,6,1}
 	}
 	return 0;
 	
