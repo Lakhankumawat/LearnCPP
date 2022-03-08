4735d908bf10dc5bb45531b0d5af56e3ede65662
tejas-tank <tejasget2learn@gmail.com>
2022-03-08 17:57:32 +0530

    add readme

--- G-GreedyAlgorithms/Maximum-Non-Overlapping-Interval.cpp
+++ G-GreedyAlgorithms/Maximum-Non-Overlapping-Interval.cpp
@@ -1,14 +1,22 @@
-// GDB CODE LINK Compile and Execute : https://onlinegdb.com/rknD8SBeO
-
+// *******************************  Maximum Non Overlapping Interval *******************************
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+
+// steps:
+// 1) Sort Interval in bases of their ending time because we have to find maximum non overlapping interval.
+// 2) then, traverse from first and remove those interval which is overlapping and subtract from the total number of intervals.
+// 3) at the end, whatever we have in count it is our ans.
+
+
+// Comparator to sort array in bases of their ending time.
 bool cmp(pair<int, int> &a, pair<int, int> &b)
-{
+{ 
     return a.second < b.second;
 }
+// Traverse the sorted array and find answer. 
 int findinterval(vector<pair<int, int>> interval, int no)
-{
+{          
     int answer = 1;
     sort(interval.begin(), interval.end(), cmp);
     int endtime = interval[0].second;
@@ -27,7 +35,7 @@ int main()
 {
     int no;
     cout << "Enter The Number of total interval :- ";
-    cin >> no;
+    cin >> no; 
     cout << endl;
     vector<pair<int, int>> interval(no);
     for (int i = 0; i < no; i++)
