f9506173fd8ae77529463fccce6c5e1a73fd99ff
Aditya Kumar Singh <78430607+adi2002vicky@users.noreply.github.com>
2022-03-10 22:38:55 -0500

    Update Morris_Traversal.cpp

--- T-Tree/T-TreeTraversals/Morris_Traversal.cpp
+++ T-Tree/T-TreeTraversals/Morris_Traversal.cpp
@@ -1,70 +1,95 @@
+/* Function to traverse binary tree without recursion and without stack */
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-struct Node { 
-	int data; 
-	struct Node* left_node; 
-	struct Node* right_node; 
-}; 
+struct Node
+{
+	int data;
+	struct Node *left_node;
+	struct Node *right_node;
+};
 
-void Morris(struct Node* root) 
-{ 
-	struct Node *curr, *prev; 
+/**
+ * @brief Morris function In this Function, links are created as successors and nodes are printed using these links. Finally, the changes are reverted back to restore the original tree.
+ *
+ * @param root
+ */
 
-	if (root == NULL) 
-		return; 
+void Morris(struct Node *root)
+{
+	struct Node *curr, *prev;
 
-	curr = root; 
+	if (root == NULL)
+		return;
+	// initialize curr equal to root
+	curr = root;
 
-	while (curr != NULL) { 
-
-		if (curr->left_node == NULL) { 
-			cout << curr->data << endl; 
-			curr = curr->right_node; 
-		} 
-
-		else { 
+	while (curr != NULL)
+	{
+		//  if left not exists
+		//  print curr
+		if (curr->left_node == NULL)
+		{
+			cout << curr->data << endl;
+			curr = curr->right_node;
+		}
+		//  if left are present
+		else
+		{
 
 			/* Find the previous (prev) of curr */
-			prev = curr->left_node; 
-			while (prev->right_node != NULL && prev->right_node != curr) {
-				prev = prev->right_node; 
-            }
+			prev = curr->left_node;
+			while (prev->right_node != NULL && prev->right_node != curr)
+			{
+				prev = prev->right_node;
+			}
 			/* Make curr as the right child of its
 			previous */
-			if (prev->right_node == NULL) { 
-				prev->right_node = curr; 
-				curr = curr->left_node; 
-			} 
+			// create temporary link between prev->right to curr
+			if (prev->right_node == NULL)
+			{
+				prev->right_node = curr;
+				curr = curr->left_node;
+			}
 
 			/* fix the right child of previous */
-			else { 
-				prev->right_node = NULL; 
-				cout << curr->data << endl; 
-				curr = curr->right_node; 
-			} 
-		} 
-	} 
-} 
+			// remove temprorary link
+			else
+			{
+				prev->right_node = NULL;
+				cout << curr->data << endl;
+				curr = curr->right_node;
+			}
+		}
+	}
+}
 
-struct Node* add_node(int data) 
-{ 
-	struct Node* node = new Node; 
-	node->data = data; 
-	node->left_node = NULL; 
-	node->right_node = NULL; 
+/**
+ * @brief add_node function are create new node
+ *
+ * @param data
+ * @return struct Node*
+ */
+struct Node *add_node(int data)
+{
+	struct Node *node = new Node;
+	node->data = data;
+	node->left_node = NULL;
+	node->right_node = NULL;
 
-	return (node); 
-} 
+	return (node);
+}
 
-int main() 
-{ 
-
-	struct Node* root = add_node(5); 
-	root->left_node = add_node(6); 
-	root->right_node = add_node(9); 
-	root->left_node->left_node = add_node(3); 
-	root->left_node->right_node = add_node(2); 
-	Morris(root); 
-	return 0; 
-} 
+// Driver Program to test above functions
+int main()
+{
+       //Tree is built like the example
+	struct Node *root = add_node(2);
+	root->left_node = add_node(5);
+	root->right_node = add_node(8);
+	root->left_node->left_node = add_node(4);
+	root->left_node->right_node = add_node(3);
+	// call morris function
+	Morris(root);
+	return 0;
+}
