d021b193578f91c242a301279a5618c296c6f14d
Jyoti Singh <singhjyoti242002@gmail.com>
2022-03-19 09:49:36 +0530

    user input of cycle linked list done

--- L-LinkedList/S-SinglyLinkedList/CycleDetection/CycleDetection.cpp
+++ L-LinkedList/S-SinglyLinkedList/CycleDetection/CycleDetection.cpp
@@ -14,36 +14,23 @@ class node {
         }
 };
 
-// function to insert node at end of linked list
-void insertAtTail(node* &head, int val){
-    node* n = new node(val);
+// function to take user input of linked list
+node* addToList(node *head){
+    bool quit = false;
+    int temp;
+    node *current = new node(-1);
 
-    if(head == NULL){
-        head = n;
-        return;
-    }
-
-    node* temp = head;
-    while(temp->next!=NULL){
-        temp = temp->next;
-    }
-    temp->next = n;
-}
-
-// Function to make cycle in linked list
-void makeCycle(node *head, int pos){
-    node *temp = head;
-    node *startNode;
-
-    int count = 1;
-    while(temp->next != NULL){
-        if(count == pos){
-            startNode = temp;
+    while (!quit){
+        cin >> temp;
+        if (temp == -1){
+            quit = true;
         }
-        temp = temp->next;
-        count++;
+        else{
+            current->data = temp;
+            current -> next = current;
+        }
     }
-    temp->next = startNode;
+    return current;
 }
 
 /* Returns true if there is a loop in linked list else returns false */
@@ -66,13 +53,8 @@ bool detectCycle(node* &head){
 int main(){
     node* head = NULL;
     // Linked list
-    insertAtTail(head, 1);
-    insertAtTail(head, 2);
-    insertAtTail(head, 3);
-    insertAtTail(head, 4);
-    insertAtTail(head, 5);
-    insertAtTail(head, 6);
-    makeCycle(head, 4);      
+    cout << "Enter the linked list items:"<<endl;
+    head = addToList(head);
 
     bool detect = detectCycle(head);
     if(detect){
