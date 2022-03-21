f16173edea4d849acaaef35db3f7d53006a9bef6
satyam singh <satyambsingh93@gmail.com>
2022-03-21 07:39:07 +0530

    adding corner case for moores voting algorithm

--- A-Array/MooresVotingAlogorithm.cpp
+++ A-Array/MooresVotingAlogorithm.cpp
@@ -11,6 +11,15 @@ Intuition:
 */
 class AlgorithmImplementation {
 public:
+    int countMajjority(int x,vector<int>&nums) {
+        // count whether we have found the correct majority element or not.
+        int count  = 0
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i] == x) count++;
+        }
+        return count > nums.size()/2;
+    }
+
     int majorityElement(vector<int>& nums) {
          // count of majority element and the majority element
         int countmajority = 0 , majorelement = 0; 
@@ -29,6 +38,8 @@ public:
             }
             
         }
+        
+        if(not countMajjority(majorelement,nums)) return -1;
         // atlast the majority will be left with us
         return majorelement;
         
