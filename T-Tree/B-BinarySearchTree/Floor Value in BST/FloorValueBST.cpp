0ac98c16a4da41066971d1178d3e96e78be05318
TRIDIB BAG <77873383+Mrjoy832@users.noreply.github.com>
2022-03-25 17:28:54 +0530

    added input format

--- T-Tree/B-BinarySearchTree/Floor Value in BST/FloorValueBST.cpp
+++ T-Tree/B-BinarySearchTree/Floor Value in BST/FloorValueBST.cpp
@@ -2,58 +2,138 @@
 /* Supoose we are given a 'key' value, The greatest value in the binary Search Tree that's smaller than the key
 is called Floor Value */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct node{ //Structure of every node of BST
+struct node
+{ // Structure of every node of BST
     int data;
-    struct node*left;
-    struct node*right;
+    struct node *left;
+    struct node *right;
 
-    node(int val){
-        data=val;
-        left=right=NULL;
+    node(int val)
+    {
+        data = val;
+        left = right = NULL;
     }
 };
 
-//Function to find floor value
-int FloorInBst(node * root, int key){
-    int floor=-1; //initial value of floor
-    while(root){
-        if(root->data==key)// if value equal to key
-          {
-              floor=root->data;
-              return floor;
-          }
-        if(key>root->data){ // if node value is less than key Go to right
-            floor=root->data;
-            root=root->right;
+// Insertion Values into BST
+node *INSERTintoBST(node *root, int val)
+{
+    if (root == NULL)
+    {
+        return new node(val);
+    }
+
+    while (true)
+    {
+        if (root->data <= val)
+        {
+            if (root->right != NULL)
+            {
+                root = root->right;
+            }
+            else
+            {
+                root->right = new node(val);
+                break;
+            }
         }
-        else{ // if node value is greater than key Go to Left
-            root=root->left;
+
+        else
+        {
+            if (root->left != NULL)
+            {
+                root = root->left;
+            }
+            else
+            {
+                root->left = new node(val);
+                break;
+            }
+        }
+    }
+}
+
+// Function to find floor value
+int FloorInBst(node *root, int key)
+{
+    int floor = -1; // initial value of floor
+    while (root)
+    {
+        if (root->data == key) // if value equal to key
+        {
+            floor = root->data;
+            return floor;
+        }
+        if (key > root->data)
+        { // if node value is less than key Go to right
+            floor = root->data;
+            root = root->right;
+        }
+        else
+        { // if node value is greater than key Go to Left
+            root = root->left;
         }
     }
     return floor;
 }
 
-int main(){
-    struct node*p=new node(10);
-    p->left=new node(5);
-    p->right=new node(15);
-    p->left->left=new node(2);
-    p->left->right=new node(8);
-    p->left->right->left=new node(6);
+int main()
+{
+    int nodes, val; //nodes=no of nodes
+    cout<<"enter the number of nodes"<<endl;
+    cin>>nodes;
+    cout<<"enter 1st value"<<endl;
+    cin>>val;
+    struct node*root=new node(val);
+    int i=nodes-1;
+    while(i){
+        cout<<"enter other  values"<<endl;
+        cin>>val;
+        INSERTintoBST(root,val);
+        i--;
+    }
+    int key;
+    cout<<"enter the key"<<endl;//input the key 
+    cin>>key;
 
-    // cout<<"the floor value of the given key is -> "<<FloorInBst(p,14)<<endl;
-    // cout<<"the floor value of the given key is -> "<<FloorInBst(p,7)<<endl;
-    cout<<"the floor value of the given key is -> "<<FloorInBst(p,1)<<endl;
-
+    cout<<"The Floor value in BST w.r.t to the key ->"<<endl;
+    cout<<FloorInBst(root,key);
 }
 
-/* OUTPUT:: 
+
+
+/* OUTPUT:
+
+
+enter the number of nodes
+6
+enter 1st value
+10
+enter other  values
+5
+enter other  values
+15
+enter other  values
+2
+enter other  values
+8
+enter other  values
+6
+enter the key
+14
+The Floor value in BST w.r.t to the key ->
+10
+
+
+*/
+
+/* BST looks Like
      10
     /  \
    5   15
-  / \  
+  / \
  2  8
    /
   6
