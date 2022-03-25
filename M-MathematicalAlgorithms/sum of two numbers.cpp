eb63ec7b9f412facab7067f910bd047a75fccce8
marwasameh2003 <101594192+marwasameh2003@users.noreply.github.com>
2022-03-26 00:08:33 +0200

    Add files via upload

---
+++ M-MathematicalAlgorithms/sum of two numbers.cpp
@@ -0,0 +1,17 @@
+// this is a program that prints the sum of two numbers
+// assignment 2 question 7
+//created on 25/03/22
+//created by MARWA SAMEH TAHA
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    double num1, num2;
+
+    cout<<"please enter the first number:";
+    cin>> num1;
+    cout<<"please enter the second number:";
+    cin>> num2;
+cout<<"the sum of "<<num1<<" and " << num2 << " is " << num2 + num1;
+}
