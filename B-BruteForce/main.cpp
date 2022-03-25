ef36a8bd7d03afbbea5f25ac640aff57a18c3282
aymanantar <53231265+aymanantar@users.noreply.github.com>
2022-03-25 17:59:30 +0200

    Added Watermelon
    simply, take the input ( the number of pieces ) , and see whether it was even or odd .
    if even , then it is "Yes"
    other wise, print "No"

---
+++ B-BruteForce/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if (n==2) cout<<"No"<<endl;
+    else if (n%2==0)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
