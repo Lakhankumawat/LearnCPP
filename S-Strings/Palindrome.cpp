0d9348ab8d8bde6e01cce2e13f3361369930afb7
Unicorn2105 <sh.unicorn2002@gmail.com>
2022-03-27 22:18:43 +0200

    Palindrome

---
+++ S-Strings/Palindrome.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string str, reversed_str;
+    cout<<"Please enter the string to check if it is palindrome or not:";
+    cin>>reversed_str;
+    str = reversed_str;
+    reverse(reversed_str.begin(),reversed_str.end());
+    if (str == reversed_str)
+    {
+        cout<<"True";   
+    }
+    else{
+        cout<<"False";
+    }
+}
