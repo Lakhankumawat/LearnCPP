77d5d620bfda0321a4701608b39a82bc13b7cb15
shahd abdelmoamen <102470488+shahdmoamen@users.noreply.github.com>
2022-03-27 20:41:11 +0200

    added two numbers

---
+++ M-MathematicalAlgorithms/add two numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n1, n2;
+    cout << "Please enter two number to get their sum\n";
+    cin >> n1 >> n2;
+    int result = n1 + n2;
+    cout<< "The result of their summation is " << result;
+    return 0;
+}
