8996f4354e18b04060a67be0d2ad65d5fe2a5dd0
maramwael188 <100848741+maramwael188@users.noreply.github.com>
2022-03-27 20:34:38 +0200

    Add files via upload

---
+++ M-MathematicalAlgorithms/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n1, n2, n3, n4, n5, n6, sum;
+    cout << "enter 6 numbers to get their sum: ";
+    cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6;
+    sum = n1 + n2 + n3 + n4 + n5 + n6;
+    cout << sum;
+}
