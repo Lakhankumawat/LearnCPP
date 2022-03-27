3b0df0af612ec46bb85f0ade593db5ebd185a904
Nada Mohammed <96924895+nadaamohhamed@users.noreply.github.com>
2022-03-27 20:26:24 +0200

    Add files via upload

---
+++ M-MathematicalAlgorithms/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n1,n2,product;
+   cout << "Please enter the 2 numbers: "<<endl;
+   cin >> n1 >> n2;
+   product = n1*n2;
+   cout << product;
+   
+}
