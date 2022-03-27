3dcf4619611d85c0be4bfb00ab21d678f3a6c5b7
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-03-27 15:20:48 +0530

    Added code for multiplication of two numbers in cpp

---
+++ Miscellaneous/Multiplication.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+	cout<<"Enter two numbers :"<<endl;
+	cin>>a>>b;
+	cout<<"Product of these two numbers:"<<a*b<<endl;
+	return 0;
+}
