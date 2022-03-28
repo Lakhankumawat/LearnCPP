cd8602807ec06baa3133ca67ca2ed394321eca9c
AyaAli <101468593+Ayaali22@users.noreply.github.com>
2022-03-28 19:09:42 +0200

    Add files via upload

---
+++ M-MathematicalAlgorithms/main (5).cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int r;
+    const float py=3.14;
+    cout<<"Enter the radius :";
+    cin>>r;
+    cout<<"The area = "<<py*r*r;
+    return 0;
+}
