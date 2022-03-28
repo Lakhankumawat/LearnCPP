efbd554e9e0f519a880c9b47ba1a044ab3864cea
ammar mahmoud <93520305+Ammar-mahmoud@users.noreply.github.com>
2022-03-28 19:44:46 +0200

    multiplication table

---
+++ M-MathematicalAlgorithms/multiplication table.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std ;
+
+int main(){
+int num;
+int i = 1 ;
+cout << "enter number : ";
+cin >> num ;
+
+while  (i <= 12)
+{
+if (i >= 10){
+cout << "* " << i << " | " << i * num << "\n" ;
+}
+if (i < 10){
+cout << "* " << i << "  | " << i * num << "\n" ;
+}
+i++ ;
+}
+return 0 ;
+}
