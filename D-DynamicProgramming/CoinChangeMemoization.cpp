29607c777675825bca9081561906bd3d8f52ea2b
Ahmed-Waleed <ahmed.ibrahim003@eng-st.cu.edu.eg>
2022-03-28 12:38:24 +0200

    Adjust Comments

--- D-DynamicProgramming/CoinChangeMemoization.cpp
+++ D-DynamicProgramming/CoinChangeMemoization.cpp
@@ -8,7 +8,7 @@ int MAX;
 
 int DFS(vector<int>& coins,int amount){
 
-    if(amount==0)         // we found an answer
+    if(amount==0)                                           // we found an answer
         return 0;
     if(amount<0)
         return MAX;
@@ -26,12 +26,12 @@ int DFS(vector<int>& coins,int amount){
 int minCoinChange(vector<int>& coins, int amount) {
 
     memo.clear();
-    memo.resize(amount+1,-1);                      // Flag all locations by -1
-    MAX = amount+1;                                          // max answer we can get is amount itself, sum of ones for example amount = 3 = 1 + 1 + 1
+    memo.resize(amount+1,-1);                               // Flag all locations by -1
+    MAX = amount+1;                                         // max answer we can get is amount itself, sum of ones for example amount = 3 = 1 + 1 + 1
 
     int minCoins = DFS(coins,amount);
 
-    return (minCoins<MAX)? minCoins:-1;                    // if answer>= MAX then we could not find a combination of coins which its sum = amount, return -1
+    return (minCoins<MAX)? minCoins:-1;                     // if answer>= MAX then we could not find a combination of coins which its sum = amount, return -1
 
 
 }
@@ -39,11 +39,11 @@ int main(){
 
     vector<int> coins1 = {1,2,5};
     int amount1 = 11;
-    cout<<minCoinChange(coins1,amount1)<<endl;  // ans = 3 , 11 = 5 + 5 + 1
+    cout<<minCoinChange(coins1,amount1)<<endl;              // ans = 3 , 11 = 5 + 5 + 1
 
     vector<int> coins2 = {2};
     int amount2 = 3;
-    cout<<minCoinChange(coins2,amount2)<<endl;  // ans = -1 , no way to construct amount = 3 form coins = [2]
+    cout<<minCoinChange(coins2,amount2)<<endl;              // ans = -1 , no way to construct amount = 3 form coins = [2]
 
     return 0;
 }
