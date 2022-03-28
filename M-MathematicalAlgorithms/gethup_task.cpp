bcec74cbf1f76ca101652b79c671578d5c54416f
marcokamal88 <102481965+marcokamal88@users.noreply.github.com>
2022-03-28 12:40:32 +0200

    add average of 3 numbers code

---
+++ M-MathematicalAlgorithms/gethup_task.cpp
@@ -0,0 +1,16 @@
+/*
+calculating average of 3 numbers
+auther : marco kamal
+date: 28/3/2022
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int num1,num2,num3,average;
+    cout<<"hi user ...input 3 numbers: ";
+    cin>>num1>>num2>>num3;
+    average=(num1+num2+num3)/3;
+    cout<<"the average of the 3 numbers u input is= "<<average;
+
+    return 0;
+    }
