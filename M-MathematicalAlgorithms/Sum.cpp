b0b624ab3972c12fb9447f1f65e6af9fa67ae4f8
NourTarek201 <92692198+NourTarek201@users.noreply.github.com>
2022-03-28 15:30:34 +0200

    Added sum

---
+++ M-MathematicalAlgorithms/Sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1,num2,result;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    result=num1+num2;
+    cout<<result;
+}
