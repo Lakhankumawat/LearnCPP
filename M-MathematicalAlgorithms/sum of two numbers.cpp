e764784b2fd2805379ae2fb89ac378037144ccb9
MR1WOLF <93862014+MR1WOLF@users.noreply.github.com>
2022-03-29 20:36:16 +0200

    Added solution of the sum of two numbers problem

---
+++ M-MathematicalAlgorithms/sum of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x,y;
+    cout<<"Enter the first number :"<<endl;
+    cin>>x;
+    cout<<"Enter the first number :"<<endl;
+    cin>>y;
+    cout<<"The sum of the two numbers is :"<<y+x;
+}
