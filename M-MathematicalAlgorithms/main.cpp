82bb4ef233780fa02260e594c28f22d6f1a20f05
MaryamSakr <102592232+MaryamSakr@users.noreply.github.com>
2022-03-29 16:37:59 +0200

    add file to sum

---
+++ M-MathematicalAlgorithms/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,sum;
+
+    cout<<"please enter first number: ";
+    cin>>x;
+    cout<<"please enter second number: ";
+    cin>>y;
+    sum=x+y;
+    cout<<"sum= "<<sum;
+}
