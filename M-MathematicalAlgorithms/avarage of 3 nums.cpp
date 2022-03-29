2ee244c3f154c13ee0c7a31c88d8acefdf99747a
saifeldeen2022 <101747344+saifeldeen2022@users.noreply.github.com>
2022-03-29 06:43:09 +0200

    avarage of 3 nums c++ code

---
+++ M-MathematicalAlgorithms/avarage of 3 nums.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n1,n2,n3,avr,sum;
+cout<<"Please enter 3 integers: "<<endl;
+cin>>n1>>n2>>n3;
+sum = n1+n2+n3;
+avr = sum/3;
+cout<<"The average = "<<avr;
+}
