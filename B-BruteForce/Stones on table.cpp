bc11b74951403e5694549796e4337aa333828a21
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2022-03-29 14:33:10 +0200

    Create Stones on table.cpp

---
+++ B-BruteForce/Stones on table.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#include <cstdlib>
+#include <cctype>
+#include<string>
+int main()
+{
+    int n,result=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    for(int i=0;i<n-1;i++){
+        if(s[i]==s[i+1])
+            result++;
+    }
+    cout<<result;
+    return 0;
+}
