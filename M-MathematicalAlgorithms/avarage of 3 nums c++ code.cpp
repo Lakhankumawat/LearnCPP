8b45e1f70b5d919dac874ec0b83231d9afa6eeb1
abdelfatah150 <92942674+abdelfatah150@users.noreply.github.com>
2022-03-29 14:48:17 +0200

    Calculate the Average

---
+++ M-MathematicalAlgorithms/avarage of 3 nums c++ code.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2 ,n3;
+
+    cout << "Enter 3 numbers integers to calculate the average: " << endl;
+    cin >> n1 >> n2 >> n3;
+
+    cout << "The Average = " << (n1+n2+n3)/3 << endl;
+}
