c1da530cf071025fbaabd681f855e7cd36a500e9
MoEhab27 <mflashcr@gmail.com>
2022-03-29 18:25:00 +0200

    Are Of Circle

---
+++ M-MathematicalAlgorithms/areOfCircle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    float r, area;
+    cout<< "Please Enter R:"<<endl;
+    cin >> r;
+
+    area = (r*r) * (22/7.0);
+    cout <<"The Area is:" << area << " Unit"<<endl;
+
+
+}
+
