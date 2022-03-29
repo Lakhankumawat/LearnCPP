ecc6135fa83423c2469a8e3650d3623397c0842f
Ayman17 <77356245+Ayman17@users.noreply.github.com>
2022-03-29 20:59:49 +0200

    Solution of perimeter problem

---
+++ M-MathematicalAlgorithms/perimeter rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width, length, result;
+    cout << "Pleas enter Width and length:";
+    cin >> width >> length;
+    result = (width * 2) + (length * 2);
+    cout << "Result: " << result;
+}
