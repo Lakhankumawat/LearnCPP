9006e7316068e2bc42f4574b7a01dee745b4bf3b
yousef1470 <93659775+yousef1470@users.noreply.github.com>
2022-03-29 04:45:19 +0200

    Area Of Circle

---
+++ M-MathematicalAlgorithms/area of the circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    float r, area;
+    cout << "please enter the radius: ";
+    cin >> r;
+    area = 3.14*r*r;
+    cout << area;
+
+    return 0;
+}
