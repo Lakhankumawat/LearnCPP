5d81964c992366b0cd42d7e64bfc9ff613d45d91
amansour13 <96149262+amansour13@users.noreply.github.com>
2022-03-29 12:03:17 +0200

    add the code of Circumference of the circle
    issue #937

---
+++ M-MathematicalAlgorithms/circumferenceOfCircle.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    float circ;
+    cout << "Please enter the radius of the cicle: ";
+    cin >> r;
+	circ = (22.0 / 7) * r; 
+    cout << "the Circumference of the circle equal: " << circ << " cm\n";
+    return 0;
+}
