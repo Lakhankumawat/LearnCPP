b87a96c6b6b7d8d2bba194cec3d183c3e5bd3915
shahdkh2288 <101909725+shahdkh2288@users.noreply.github.com>
2022-03-29 14:06:27 +0200

    added perimeter of rectangle

---
+++ M-MathematicalAlgorithms/perimeter of rectangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int l,w;
+    cout<<"please enter the length and the width of rectangle"<<"\n";
+    cin>>l>>w;
+    cout<<"the perimeter of rectangle is: "<<(l+w)*2<<" cm";
+    return 0;
+
+
+}
+
