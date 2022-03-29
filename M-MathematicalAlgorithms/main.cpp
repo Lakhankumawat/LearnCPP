b1e23ecdc5d53bdd0e24aa0d7c9bc75172cf81d8
Ahmed Adel <90573805+Dola1122@users.noreply.github.com>
2022-03-29 14:16:53 +0200

    add Find New price

---
+++ M-MathematicalAlgorithms/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int price ;
+    double discountPercentage;
+    cin>>price>>discountPercentage;
+    cout<<price*(discountPercentage/100);
+
+    return 0;
+}
