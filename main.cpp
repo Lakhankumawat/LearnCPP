b753f1ce7d0821b51dd7ecc8a449df32d8a03e87
MoazAlaa1 <102490051+MoazAlaa1@users.noreply.github.com>
2022-03-29 23:18:54 +0200

    Add area of rectangle

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int length, width ;
+    cout <<"this program calculate area of rectangle \n ";
+    cout <<"pleas enter rectangle length \n ";
+    cin >> length ;
+    cout <<"please enter rectangle width \n ";
+    cin >> width;
+    cout <<"area of rectangle equal "<< length*width <<"\n";
+    return 0;
+}
