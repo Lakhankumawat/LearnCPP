a42c482e487e6eaf34dcec44c4bb5a07c3bbd182
OmarAmmarII <102245001+OmarAmmarII@users.noreply.github.com>
2022-03-30 16:26:11 +0200

    Add files via upload

---
+++ M-MathematicalAlgorithms/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x, y , z;
+   cout << "enter 3 numbers" << endl;
+   cin >> x >> y >> z;
+   int sum = x + y + z;
+   cout << "the average = " << sum /3;
+}
