1c72524a88e12b74fc895fdbce84152c6f64d357
Ahmedreda122 <66702788+Ahmedreda122@users.noreply.github.com>
2022-03-30 09:09:20 +0200

    Square of Stars #1005
    you have to enter the length of a square and it print a  square of stars of that length

---
+++ P-Problem Statements & Solution/1-Level 1/square.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int length;
+
+	cout << "Please, Enter the length: ";
+
+	cin >> length;
+
+	for (int i = 0; i < length; ++i)
+	{
+		for (int j = 0; j < length; ++j)
+		{
+			cout << '*';
+		}
+
+		cout << '\n';
+	}
+	
+}
