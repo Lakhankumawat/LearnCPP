2cebc7c8f93120d505b11fdd3d58220e28328f2c
Omar Rabea shaban Bayoumi <101938505+3omarrabi3@users.noreply.github.com>
2022-03-30 17:42:31 +0200

    Calculating _The_Power

---
+++ M-MathematicalAlgorithms/Calculating _The_Power.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+  // computes 5 raised to the power 3
+  int x,y;
+  cout <<"ENTER THE BASE "<<endl;
+  cin>>x;
+  cout<<"ENTER THE POWER "<<endl;
+  cin>>y;
+  cout <<"THE RESULT IS = "<< pow(x, y)<<endl;
+
+  return 0;
+}
