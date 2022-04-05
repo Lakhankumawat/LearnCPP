c711874e9c99841aab86b6bca25a6b63fbc785f1
Uhini Mukherjee <uhinimukherjee@gmail.com>
2022-04-05 21:02:16 +0530

    The comments are added.

--- T-Tree/F-FenwickTree/K_Query.cpp
+++ T-Tree/F-FenwickTree/K_Query.cpp
@@ -19,12 +19,14 @@ bool compare(event a,event b){
 
 lli BIT[300005];
 lli ans[200005];
+//the update function
 void update(lli p,lli val){
     while(p<300005){
         BIT[p]+=val;
         p+=p&(-p);
     }
 }
+//the query function
 lli queri(lli p){
     lli sum=0;
     while(p){
@@ -42,6 +44,7 @@ int main()
         scanf("%lld",&a[i].v);
         a[i].idx=i;
     }
+    //sorting needed so that the bits where they are stored can be maintained according to the index
     sort(a+1,a+n+1,com);
     lli q;
     scanf("%lli",&q);
@@ -51,12 +54,15 @@ int main()
         //cin>>t[i].l>>t[i].r>>t[i].value;
         t[i].ind=i;
     }
+    //the qeueries are sorted so that there are no overlaps
     sort(t,t+q,compare);
     lli g=1;
     for(int i=0;i<q;i++){
         
      while(a[g].v>t[i].value)
+     //the update query
        {update(a[g].idx,1);g++;}
+       //the sum query
         	ans[t[i].ind]=queri(t[i].r)-queri(t[i].l-1);
     }
     for(int i=0;i<q;i++){
