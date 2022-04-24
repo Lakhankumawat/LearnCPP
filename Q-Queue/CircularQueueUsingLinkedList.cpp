19bf315717715c539c54d9db88fd00b6cf4b40c8
Sukriti-m <sukritimaurya.fzd@gmail.com>
2022-04-24 11:45:01 +0530

    circ_queue_ll

--- Q-Queue/CircularQueueUsingLinkedList.cpp
+++ Q-Queue/CircularQueueUsingLinkedList.cpp
@@ -10,33 +10,37 @@ struct node *rear=NULL;
 void Insert(){int item;
 struct node *newNode;
 newNode= (struct node *)malloc(sizeof(struct node));
-cout<<"Insert item in the queue"<<endl;
+cout<<"Insert item in the circular queue"<<endl;
 cin>>item;
 newNode ->data=item;
 newNode ->next=NULL;
 if((front==NULL) && (rear==NULL))
 {
     front = rear = newNode;
+    rear->next=front;
     return;
 }
 rear->next=newNode;
 rear= newNode;
+rear->next=front;
 
 }
+
 
 
 void Delete(){
     struct node *temp=front;
     if(front==NULL)
-    {cout<<"Queue Underflow"<<endl;
+    {cout<<"Circular Queue Underflow"<<endl;
         return;}
     if(front==rear)
-    {cout<<"Item deleted from queue is : "<<front->data<<endl;
+    {cout<<"Item deleted from circular queue is : "<<front->data<<endl;
 
         front= rear= NULL;
     }
-    else{cout<<"Item deleted from queue is : "<<front->data<<endl;
+    else{cout<<"Item deleted from circular queue is : "<<front->data<<endl;
         front= front->next;
+        rear->next=front;
     }
     free(temp);
 
@@ -45,24 +49,25 @@ void Delete(){
 void Display(){
     struct node *temp=front;
  if ((front == NULL) && (rear == NULL)) {
-      cout<<"Queue is empty"<<endl;
+      cout<<"Circular Queue is empty"<<endl;
       return;
    }
 
-cout<<"Items present in the queue are: ";
-    while(temp!= NULL){
+cout<<"Items present in the circular queue are: ";
+    while(temp->next != front){
         cout<<temp->data<<" ";
         temp=temp->next;
     }
+    cout<<temp->data<<" ";
     cout<<endl;
 }
 
 
 int main(){
     int choice;
-    cout<<"1. Insert an item to queue"<<endl;
-    cout<<"2. Delete an item from queue"<<endl;
-    cout<<"3. Display all items of queue"<<endl;
+    cout<<"1. Insert an item to circular queue"<<endl;
+    cout<<"2. Delete an item from circular queue"<<endl;
+    cout<<"3. Display all items of circular queue"<<endl;
     cout<<"4. Exit"<<endl;
     do{cout<<"Enter choice: "<<endl;
     cin>>choice;
