438cc965830b9b025d1e5961c14f9ce9f416eae0
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2022-04-30 09:42:33 +0530

    Update CoinChange.cpp

--- D-DynamicProgramming/CoinChange.cpp
+++ D-DynamicProgramming/CoinChange.cpp
@@ -1,21 +1,29 @@
+//COIN CHANGE
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int minCoins(int currentCoin,int amount,vector<int>&coins,int n,vector<vector<int>>&v)
 {
+     //base conditions
      if(amount == 0) return 0;
         
     if(currentCoin >= n) return 100001;
         
+    //if value is already present in the vector v ,then extract from it, no need to make recursive calls for it 
     if(v[currentCoin][amount] != -1)
         return v[currentCoin][amount];
-        
+       
+     //Explore the two possibilities :- 1)take the coin ,2)don't take the coin
     int consider = 100001;
+     
+     //we'll consider a coin only when its denomination is either less than or equal to the amount
     if(coins[currentCoin] <= amount)
         consider = 1 + minCoins(currentCoin,amount-coins[currentCoin],coins,n,v);
         
     int notConsider = minCoins(currentCoin+1,amount,coins,n,v);
-        
+       
+    //storing the answer for the current coordinate in v,so that we can use it in future
     v[currentCoin][amount] = min(consider,notConsider);
     return v[currentCoin][amount];
 }
