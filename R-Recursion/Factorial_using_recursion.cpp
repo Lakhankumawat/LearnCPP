f909a9d62c96d70785997b29065d564c9eda9501
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2022-05-05 21:58:47 +0530

    Created Factorial Using Recursion File
    This file explains the calculation of Factorial of a number using recursion.

---
+++ R-Recursion/Factorial_using_recursion.cpp
@@ -0,0 +1,24 @@
+/*                                       FACTORIAL NUMBER PROGRAM
+TIME COMPLEXITY : O(N)
+SPACE COMPLEXITY : O(N)    Here, N is recursion stack space.
+Link to the article for reference : https://www.sciencedirect.com/topics/computer-science/factorial-function#:~:text=The%20factorial%20function%20can%20be,factorial(n%20%E2%80%93%201).        */
+#include<iostream>
+using namespace std;
+// Function to calculate factorial of a number
+int factorial(int n) {
+
+ if (n <= 1) //base case
+
+ return 1;
+
+ else
+
+ return (n * factorial(n - 1));  //recursive call
+
+}
+int main(){
+	int x;
+	cout<<"Please enter the number, whose factorial is needed "<<endl;
+	cin>>x;
+	cout<<"The factorial of number "<<x<<" is "<<factorial(x)<<endl;
+}
