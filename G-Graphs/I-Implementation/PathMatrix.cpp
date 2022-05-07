03b8242d4c674d3980b1a89b15ad3decc72a8dc0
VaishnaviMandloi <vaishnavimandloi2003@gmail.com>
2022-05-07 20:46:29 +0530

    added comment ,updated readme ,change file location

--- G-Graphs/I-Implementation/PathMatrix.cpp
+++ G-Graphs/I-Implementation/PathMatrix.cpp
@@ -3,8 +3,8 @@
 #include<iostream>
 
 using namespace std;
-#define MAX 100
-int adj[MAX][MAX];
+#define MAX 100 // defining MAX as 100
+int adj[MAX][MAX]; // declaring adj[][] 2d array globally
 void display(int matrix[MAX][MAX]);
 void create();
 void power_matrix(int p , int result[MAX][MAX]);
@@ -13,29 +13,29 @@ int n;
 
 int main()
 {
-    int result[MAX][MAX];
-    int x[MAX][MAX],path[MAX][MAX],i,j,power;
-    create();
+    int result[MAX][MAX];  // declaring result materix (2d array) for storing the power adjacency matrix
+    int x[MAX][MAX],path[MAX][MAX],i,j,power; // declaring path matrix to store the entries as 1 path present & x matrix for end result
+    create(); // create function to create graph adjacency matrix
     cout<<"adjacency matrix : "<<endl;
-    display(adj);
+    display(adj); // display the user defined graph adjacency matrix
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
-            x[i][j]=0;
+            x[i][j]=0; // initialising entires as zero
         }
     }
-    for(power=1;power<=n;power++){
+    for(power=1;power<=n;power++){ // calulating result matrix for each power of adjacency matrix
         power_matrix(power,result);
         cout<<"adjacency matrix raised to power "<<power<<" : "<<endl;
-        display(result);
+        display(result); // displaying result matrix for each power of adjacency matrix
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){ 
-                x[i][j] = x[i][j] + result[i][j]; 
+                x[i][j] = x[i][j] + result[i][j]; // upadating by adding to get total paths
             }
         }
     }
     cout<<"Result matrix is : "<<endl;
-    display(x);
-    for(i=0;i<n;i++){
+    display(x); // matrix showing total paths between vertices
+    for(i=0;i<n;i++){ //updating path matrix to store the entries as 1 if atleast 1 path present between vertices
         for(j=0;j<n;j++){
             if(x[i][j]==0)
              path[i][j]=0;
@@ -44,11 +44,11 @@ int main()
         }
     }
     cout<<"the path matrix is : "<<endl;
-    display(path);
+    display(path); // displaying path matrix
     
     return 0;
 }
-void create(){
+void create(){ // create function to create graph adjacency matrix
     int max_edges,i,origin,destin;
     cout<<"Enter number of vertices : ";
     cin>>n;
@@ -71,12 +71,12 @@ void create(){
 // compute pth power of matrix and store
 void power_matrix(int p , int result[MAX][MAX]){
     int i,j,k,tmp[MAX][MAX];
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++){ //copy all the entrires of adjacency matrix to result matrix
         for(j=0;j<n;j++){
             result[i][j]=adj[i][j];
         }
     }
-    for(k=1;k<p;k++){
+    for(k=1;k<p;k++){//obtain matrices for all powers .
         multiply(result,adj,tmp);
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){
@@ -87,6 +87,7 @@ void power_matrix(int p , int result[MAX][MAX]){
 }
 
 void multiply(int matrix1[MAX][MAX] ,int matrix2[MAX][MAX] ,int result_matrix[MAX][MAX]){
+    //by multiply result matrix obtain in each step by adjacency matrix .
     int i,j,k;
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
@@ -98,7 +99,7 @@ void multiply(int matrix1[MAX][MAX] ,int matrix2[MAX][MAX] ,int result_matrix[MA
     }
 }
 
-void display(int matrix[MAX][MAX]) {
+void display(int matrix[MAX][MAX]) {// displaying matrix
     int i , j ;
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
