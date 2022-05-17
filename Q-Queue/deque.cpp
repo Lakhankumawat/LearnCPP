075f62d97302db88ed611547b890c629dd45aa98
Ayushi Bisht <94152085+AyuBisht@users.noreply.github.com>
2022-05-17 11:24:51 +0530

    added comments

--- Q-Queue/deque.cpp
+++ Q-Queue/deque.cpp
@@ -9,6 +9,8 @@ public:
         f=-1;
         r=-1;
     }
+   
+    //inserts element at the front if the deque is not full
     void insert_at_beg(int i) {
         if(f==-1) {
             f=0;
@@ -22,7 +24,7 @@ public:
         cout<<"\n insertion is not possible, overflow!!!";
         }
     }
-
+    //inserts element at the rear if the deque is not full
     void insert_at_end(int i){
         if(r>=SIZE-1) {
             cout<<"\n insertion is not possible, overflow!!!!";
@@ -38,9 +40,9 @@ public:
             a[r]=i;
         }
     }
-
+    // deletes element from the front if the deque is not empty
     void delete_fr_front(){
-        if(f==-1) {
+       if(f==-1) {
             cout<<"deletion is not possible::dequeue is empty";
             return;
         }
@@ -53,8 +55,8 @@ public:
                 f=f+1;
         }
     }
-
-    void delete_fr_rear(){
+    // deletes element from the end if the deque is not empty
+    void delete_fr_rear(){        
         if(f==-1) {
             cout<<"deletion is not possible::dequeue is empty";
             return;
@@ -66,6 +68,7 @@ public:
                 r=r-1;
         }
     }
+    //prints deque if it is not empty
     void show(){
         if(f==-1) {
             cout<<"Dequeue is empty";
@@ -79,30 +82,31 @@ public:
     }
 };
 
+//Driver program 
 int main() {
     int i;
     dequeue d;
-    
+    //adds element at the front
     cout<<"enter the element to be inserted at beginning ";
     cin>>i;
     d.insert_at_beg(i);
-
+    //adds element at the back
     cout<<"enter the element to be inserted at end";
     cin>>i;
     d.insert_at_end(i);
-
+    //prints deque  
     cout<<"deque is : ";
     d.show();
-
+    //deletes from the front
     cout<<"deletion from front"<<endl;
     d.delete_fr_front();
-
+    //prints deque  
     cout<<"deque is : ";
     d.show();
-    
+    //deletes from the back
     cout<<"deletion from rear"<<endl;
     d.delete_fr_rear();
-
+    //prints deque (2 elements added and 2 deleted thus deque should be empty)
     cout<<"deque is : ";
     d.show();
 }
